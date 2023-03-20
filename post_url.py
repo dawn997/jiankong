fcd4c1ae1a7c8d74da91077723998e4e072ce678
dawn997 <wx1543121128@gmail.com>
2023-03-20 21:19:01 +0800

    微信消息推送

--- post_url.py
+++ post_url.py
@@ -1,13 +1,17 @@
 import requests
 import json
+import os
 def post_wechat(filename):
+    wx_push_apptoken = os.environ.get("WX_PUSH_APPTOKEN")
+    uid = os.environ.get("WX_PUSH_UID")
+    img_url = os.environ.get("IMAGE_URL")
     headers = {
         "Content-Type": "application/json; charset=UTF-8"
     }
     url = "https://wxpusher.zjiecode.com/api/send/message"
     pyload = {
-        "appToken": "AT_BA8E2Mfd8vRwuiWlbnAMQnx20EaFwgdp",
-        "content": "http://test.luckytnt.top:8881/map/" + filename + ".jpg",
+        "appToken": wx_push_apptoken,
+        "content": img_url + filename + ".jpg",
         "summary": "消息摘要",
         # 消息摘要，显示在微信聊天页面或者模版消息卡片上，限制长度100，可以不传，不传默认截取content前面的内容。
         "contentType": 2,
@@ -17,9 +21,9 @@ def post_wechat(filename):
         ],
         "uids": [
             # 发送目标的UID，是一个数组。注意uids和topicIds可以同时填写，也可以只填写一个。
-            "UID_E0zrUrJZVMMQvglmg6VRys376cqo"
+            uid
         ],
-        "url": "http://test.luckytnt.top:8881/map/" + filename + ".jpg",
+        "url": img_url + filename + ".jpg",
         # //原文链接，可选参数
         "verifyPay": False
         # 是否验证订阅时间，true表示只推送给付费订阅用户，false表示推送的时候，不验证付费，不验证用户订阅到期时间，用户订阅过期了，也能收到。
